4a82dcdd13046abb648e202a644fecfd3a625f86
minkukjo <strike0115@naver.com>
2020-11-22 15:09:12 +0900

    Updates

---
+++ precache-manifest.604e7380008799ae565d6614a46a5a8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5496b43904de245bb4355c4a61d0bec",
+    "url": "/8th-final-3team-front/index.html"
+  },
+  {
+    "revision": "c305474d82c66bd211ec",
+    "url": "/8th-final-3team-front/static/css/main.3e4dd1d8.chunk.css"
+  },
+  {
+    "revision": "24879f9202addb0a7d2b",
+    "url": "/8th-final-3team-front/static/js/2.aa4bf471.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/8th-final-3team-front/static/js/2.aa4bf471.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c305474d82c66bd211ec",
+    "url": "/8th-final-3team-front/static/js/main.15a7aa20.chunk.js"
+  },
+  {
+    "revision": "b0243b2ed61edccdea68",
+    "url": "/8th-final-3team-front/static/js/runtime-main.77a459ee.js"
+  },
+  {
+    "revision": "147b9116752c1b983d64b41147aabd21",
+    "url": "/8th-final-3team-front/static/media/6wheel_nomal.147b9116.svg"
+  }
+]);
